0718ae483d5d02f4e05ec9fe4aa737e464d4d567
ngsok <nagasako1128@gmail.com>
2018-06-13 14:05:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -75,8 +75,9 @@ def callback():
                         'type': receive_json['events'][0]['source']['type']
                     },
                     'timestamp': receive_json['events'][0]['timestamp'],
-                    'messages': {
+                    'message': {
                         'type': 'text',
+                        'id': receive_json['events'][0]['message']['id'],
                         'text': '図書館にそんな本ねーよ！アマゾンで買え！https://www.amazon.co.jp/s/ref=nb_sb_noss_1/358-0096124-2258834?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&url=search-alias%3Daps&field-keywords=' + line_input_message
                     }
                 }]
